0baed48d9d4181f00c6f7f798304c617817f1b52
Abylaikhan444 <fifa77882@gmail.com>
2026-06-13 19:32:13 +0500

    Finished the full APP!

--- js/script.js
+++ js/script.js
@@ -19,8 +19,6 @@ const humidity = document.querySelector('#humidity');
 //^    - использует fetch() + await
 //^    - возвращает данные
 
-const cityName = inputCity.textContent;
-
 async function getWeather(city) {
     const response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric&lang=ru`);
 
@@ -28,10 +26,86 @@ async function getWeather(city) {
         throw new Error('Город не найден!');
     }
 
-
+    const responseResult = await response.json();
+    return responseResult;
 }
 
 
-console.log(cityName);
 
+//& 3. Напиши функцию showResult(data) которая:
+//^    - показывает блок #result
+//^    - заполняет cityName — data.name
+//^    - заполняет temp — data.main.temp + '°C'
+//^    - заполняет description — data.weather[0].description
+//^    - заполняет humidity — 'Влажность: ' + data.main.humidity + '%'
 
+function showResult(data) {
+    //! 1. убрать класс hidden у result
+    result.classList.remove('hidden');
+    //! 2. cityName.textContent = ?
+    cityName.textContent = data.name;
+    //! 3. temperature.textContent = ?
+    temperature.textContent = data.main.temp + '°C';
+    //! 4. description.textContent = ?
+    description.textContent = data.weather[0].description;
+    //! 5. humidity.textContent = ?
+    humidity.textContent = 'Влажность: ' + data.main.humidity + '%';
+}
+
+//& 4. Напиши функцию showError(message) которая:
+//^    - показывает блок #error с текстом ошибки
+
+function showError(message) {
+    error.classList.remove('hidden');
+    error.textContent = message;
+}
+
+//& 5. Напиши функцию handleSearch() которая:
+//^    - берёт значение из input
+//^    - если пустое — ничего не делает
+//^    - показывает лоадер, скрывает result и error
+//^    - вызывает getWeather()
+//^    - показывает результат или ошибку
+//^    - скрывает лоадер
+
+async function handleSearch() {
+    const city = inputCity.value.trim();
+
+    //! 1. если city пустой — выходим
+    if (city === '') {
+        return;
+    }
+
+    //! 2. показать loader, скрыть result и error
+    //!    loader.classList.remove('hidden')
+    //!    result.classList.add('hidden')
+    //!    error.classList.add('hidden')
+    loader.classList.remove('hidden');
+    result.classList.add('hidden');
+    error.classList.add('hidden');
+    
+    //! 3. try/catch:
+    //!    try — вызвать getWeather(city), передать результат в showResult()
+    //!    catch — передать err.message в showError()
+    try {
+        const getWeatherResult = await getWeather(city);
+        showResult(getWeatherResult);
+    } catch (err) {
+        showError(err.message);
+    } finally {
+        loader.classList.add('hidden');
+    }
+    // 4. в finally — скрыть loader
+}
+
+//& 6. Повесь события:
+//!    - клик на кнопку → handleSearch()
+searchButton.addEventListener('click', () => {
+    handleSearch();
+})
+//!    - Enter в input → handleSearch()
+inputCity.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        handleSearch();
+    }
+});
